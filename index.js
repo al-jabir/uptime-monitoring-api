c6425e62d3aaeb871691cd3f2f4323f69f46501a
al-jabir <aljabir098@gmail.com>
2022-11-05 20:02:43 +0600

    done

--- index.js
+++ index.js
@@ -1,54 +1,20 @@
 const http = require("http");
-const url = require("url");
-const {StringDecoder} = require('string_decoder')
+const {handleReqRes} = require('./helpers/handleReqRes')
 
-const app = {};
-
-// configuration
+const app = {}
 
 app.config = {
-  port: 2000,
-};
+  port: 1998
+}
 
 app.createServer = () => {
-  const server = http.createServer(app.handleServer);
-  // server host out
-  server.listen(app.config.port, () => {
-    console.log(`The server is runnig ${app.config.port}`);
-  });
-};
-
-app.handleServer = (req, res) => {
-  const parseUrl = url.parse(req.url, true);
-
-  const path =parseUrl.pathname
-
-  const trimmedPath = path.replace(/^\/+|\/+$/g,'');
-
-  const method = req.method.toLowerCase();
-
-  const queryStringObj = parseUrl.query;
-
-  const headearObject = req.headers;
-
-  const decoder = new StringDecoder('utf-8')
-  let realData = ''
-
-  req.on('data',(buffer)=>{
-    realData+= decoder.write(buffer)
+  const server = http.createServer(app.handlerServer);
+  server.listen(app.config.port,() => {
+    console.log(`The server is running port: ${app.config.port}`)
   })
+}
 
-  req.on('end',()=>{
-    realData+= decoder.end()
-
-    console.log(realData)
-
-    res.end("I will be real programmers");
-  })
+app.handlerServer = handleReqRes;
 
 
-
-};
-
-// server start
-app.createServer();
+app.createServer()
