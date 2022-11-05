7cccaeedc901582d493e3b138b965e5f4aff82f9
al-jabir <aljabir098@gmail.com>
2022-11-06 02:06:14 +0600

    done

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const http = require("http");
 const { handleReqRes } = require("./helpers/handleReqRes");
 const environment = require("./helpers/environtments");
+const data = require("./lib/data");
 
 const app = {};
+
+// data.create(
+//   "test",
+//   "newFile",
+//   { name: "Bangladesh", language: "Bangla" },
+//   (err) => {
+//     console.log(`error was`, err);
+//   }
+// );
+
+// data.read("mafia", "newMafia", (err, result) => {
+//   console.log(err, result);
+// });
+
+data.read("king", "newKing", { country: "England" }, (err) => {
+  console.log(err);
+});
 
 app.createServer = () => {
   const server = http.createServer(app.handlerServer);
