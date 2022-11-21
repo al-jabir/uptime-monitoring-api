0304141cc8828fd0081824f116162a573ffcc72a
al-jabir <aljabir098@gmail.com>
2022-11-21 20:36:40 +0600

    added uerHandler

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,9 @@
 const { mafiaHandler } = require("../handlers/routeHandle/sampleHandler");
-
+const { userHandler } = require("../handlers/routeHandle/userHandler");
 const routes = {
   mafia: mafiaHandler,
   production: mafiaHandler,
+  user: userHandler,
 };
 
 module.exports = routes;
