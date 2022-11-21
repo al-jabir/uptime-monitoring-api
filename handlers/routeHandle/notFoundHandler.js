26aefd048d0bf03b6a4e2fe2c09ecec2969cda23
al-jabir <aljabir098@gmail.com>
2022-11-21 09:52:23 +0600

    done

--- handlers/routeHandle/notFoundHandler.js
+++ handlers/routeHandle/notFoundHandler.js
@@ -1,7 +1,6 @@
 const handler = {};
 
 handler.notFoundHandler = (reqeustProperties, callback) => {
-  console.log(reqeustProperties);
   callback(404, {
     message: "Your requested URL was not found",
   });
