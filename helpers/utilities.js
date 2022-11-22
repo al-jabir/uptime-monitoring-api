7a4aab8b3c4f0d29f3e884c28f9b0951dda67fc3
al-jabir <aljabir098@gmail.com>
2022-11-22 21:26:51 +0600

    hash password func

--- helpers/utilities.js
+++ helpers/utilities.js
@@ -1,4 +1,8 @@
+const crypto = require("crypto");
 const utilities = {};
+const environment = require("../helpers/environtments");
+
+// parse JSON string to object
 
 utilities.parseJSON = (jsonString) => {
   let output = {};
@@ -10,6 +14,18 @@ utilities.parseJSON = (jsonString) => {
   }
 
   return output;
+};
+
+// hash string
+utilities.hash = (str) => {
+  if (typeof str === "string" && str.length > 0) {
+    const hash = crypto
+      .createHmac("sha256", environment[process.env.NODE_ENV].secretKey)
+      .update(str)
+      .digest("hex");
+    return hash;
+  }
+  return false;
 };
 
 module.exports = utilities;
