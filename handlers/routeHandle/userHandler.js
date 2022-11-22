a006258a2fbec1e12a4521457537ec99bcb23aa0
al-jabir <aljabir098@gmail.com>
2022-11-22 18:32:50 +0600

    hanlder create add own method

--- handlers/routeHandle/userHandler.js
+++ handlers/routeHandle/userHandler.js
@@ -1,7 +1,27 @@
 const handler = {};
 
 handler.userHandler = (requestProperties, callback) => {
-  callback(200, { message: "This is user url" });
+  const acceptedMethods = ["get", "post", "put", "delete"];
+  if (acceptedMethods.indexOf(requestProperties.method) > -1) {
+    handler._user[requestProperties.method](requestProperties, callback);
+  } else {
+    callback(405);
+  }
+  // callback(200, { message: "This is user url" });
 };
+
+handler._user = {};
+
+handler._user.post = (requestProperties, callback) => {};
+
+handler._user.get = (requestProperties, callback) => {
+  callback(200, {
+    message: "I will be wait for api",
+  });
+};
+
+handler._user.put = (requestProperties, callback) => {};
+
+handler._user.delete = (requestProperties, callback) => {};
 
 module.exports = handler;
