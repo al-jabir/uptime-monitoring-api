08d514ae6e4d90e879d1cff9b4cb8a6183a161ae
al-jabir <aljabir098@gmail.com>
2022-11-22 22:27:14 +0600

    tosAgreement fixed

--- handlers/routeHandle/userHandler.js
+++ handlers/routeHandle/userHandler.js
@@ -45,7 +45,7 @@ handler._users.post = (requestProperties, callback) => {
 
   const tosAgreement =
     typeof requestProperties.body.tosAgreement === "boolean" &&
-    requestProperties.body.tosAgreement.trim().length > 0
+    requestProperties.body.tosAgreement
       ? requestProperties.body.tosAgreement
       : false;
 
@@ -88,33 +88,104 @@ handler._users.post = (requestProperties, callback) => {
   }
 };
 
-handler._users.get = (requestProperties, callback) => {
-  const phone =
-    typeof requestProperties.queryStringObject.phone === "string" &&
-    requestProperties.queryStringObject.phone.trim().length === 11
-      ? requestProperties.queryStringObject.phone
-      : false;
-  if (phone) {
-    // lookup the user
-    data.read("users", phone, (err, u) => {
-      const user = { ...parseJSON(u) };
-      if (!err && user) {
-        delete user.password;
-        callback(200, user);
-      } else {
-        callback(404, {
-          error: "Requested user was not found!",
-        });
-      }
-    });
-  } else {
-    callback(404, {
-      error: "Requested user was not found!",
-    });
-  }
-};
+// handler._users.get = (requestProperties, callback) => {
+//   const phone =
+//     typeof requestProperties.queryStringObject.phone === "string" &&
+//     requestProperties.queryStringObject.phone.trim().length === 11
+//       ? requestProperties.queryStringObject.phone
+//       : false;
+//   if (phone) {
+//     // lookup the user
+//     data.read("users", phone, (err, u) => {
+//       const user = { ...parseJSON(u) };
+//       if (!err && user) {
+//         delete user.password;
+//         callback(200, user);
+//       } else {
+//         callback(404, {
+//           error: "Requested user was not found!",
+//         });
+//       }
+//     });
+//   } else {
+//     callback(404, {
+//       error: "Requested user was not found!",
+//     });
+//   }
+// };
 
-handler._users.put = (requestProperties, callback) => {};
+// handler._users.put = (requestProperties, callback) => {
+//   // check the phone number if valid
+//   const phone =
+//     typeof requestProperties.body.phone === "string" &&
+//     requestProperties.body.phone.trim().length === 11
+//       ? requestProperties.body.phone
+//       : false;
+
+//   const firstName =
+//     typeof requestProperties.body.firstName === "string" &&
+//     requestProperties.body.firstName.trim().length > 0
+//       ? requestProperties.body.firstName
+//       : false;
+
+//   const lastName =
+//     typeof requestProperties.body.lastName === "string" &&
+//     requestProperties.body.lastName.trim().length > 0
+//       ? requestProperties.body.lastName
+//       : false;
+
+//   const password =
+//     typeof requestProperties.body.password === "string" &&
+//     requestProperties.body.password.trim().length > 0
+//       ? requestProperties.body.password
+//       : false;
+
+//   if (phone) {
+//     if (firstName || lastName || password) {
+//       // loopkup the user
+//       data.read("users", phone, (err1, uData) => {
+//         const userData = { ...parseJSON(uData) };
+
+//         if (!err1 && userData) {
+//           if (firstName) {
+//             userData.firstName = firstName;
+//           }
+//           if (lastName) {
+//             userData.firstName = firstName;
+//           }
+//           if (password) {
+//             userData.password = hash(password);
+//           }
+
+//           // store to database
+//           data.update("users", phone, userData, (err2) => {
+//             if (!err2) {
+//               callback(200, {
+//                 message: "User was updated successfully!",
+//               });
+//             } else {
+//               callback(500, {
+//                 error: "There was a problem in the server side!",
+//               });
+//             }
+//           });
+//         } else {
+//           callback(400, {
+//             error: "You have a problem in your request!",
+//           });
+//         }
+//       });
+//     } else {
+//       callback(400, {
+//         error: "You have a problem in your request!",
+//       });
+//     }
+//   } else {
+//     callback(400, {
+//       error: "Invalid phone number. Please try again!",
+//     });
+//   }
+// };
 
 handler._users.delete = (requestProperties, callback) => {};
 
