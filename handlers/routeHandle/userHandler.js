c8ae262a8fecdc835300d7c4da6bf8d48b2b3180
al-jabir <aljabir098@gmail.com>
2022-11-22 22:12:24 +0600

    users.post

--- handlers/routeHandle/userHandler.js
+++ handlers/routeHandle/userHandler.js
@@ -1,27 +1,99 @@
+// dependencies
+
+const data = require("../../lib/data");
+const { hash } = require("../../helpers/utilities");
+const { parseJSON } = require("../../helpers/utilities");
+
 const handler = {};
 
 handler.userHandler = (requestProperties, callback) => {
   const acceptedMethods = ["get", "post", "put", "delete"];
   if (acceptedMethods.indexOf(requestProperties.method) > -1) {
-    handler._user[requestProperties.method](requestProperties, callback);
+    handler._users[requestProperties.method](requestProperties, callback);
   } else {
     callback(405);
   }
   // callback(200, { message: "This is user url" });
 };
 
-handler._user = {};
+handler._users = {};
 
-handler._user.post = (requestProperties, callback) => {};
+handler._users.post = (requestProperties, callback) => {
+  const firstName =
+    typeof requestProperties.body.firstName === "string" &&
+    requestProperties.body.firstName.trim().length > 0
+      ? requestProperties.body.firstName
+      : false;
 
-handler._user.get = (requestProperties, callback) => {
-  callback(200, {
-    message: "I will be wait for api",
-  });
+  const lastName =
+    typeof requestProperties.body.lastName === "string" &&
+    requestProperties.body.lastName.trim().length > 0
+      ? requestProperties.body.lastName
+      : false;
+
+  const phone =
+    typeof requestProperties.body.phone === "string" &&
+    requestProperties.body.phone.trim().length === 11
+      ? requestProperties.body.phone
+      : false;
+
+  const password =
+    typeof requestProperties.body.password === "string" &&
+    requestProperties.body.password.trim().length > 0
+      ? requestProperties.body.password
+      : false;
+
+  const tosAgreement =
+    typeof requestProperties.body.tosAgreement === "boolean" &&
+    requestProperties.body.tosAgreement.trim().length > 0
+      ? requestProperties.body.tosAgreement
+      : false;
+
+  // check condition
+
+  if (firstName && lastName && password && tosAgreement) {
+    // make sure that the user dosen't already exits
+    data.read("users", phone, (err1) => {
+      if (err1) {
+        const userObject = {
+          firstName,
+          lastName,
+          phone,
+          password: hash(password),
+          tosAgreement,
+        };
+        // store the user to db
+
+        data.create("users", phone, userObject, (err2) => {
+          if (!err2) {
+            callback(200, {
+              message: "User was created successfully!",
+            });
+          } else {
+            callback(500, {
+              error: "Could not create user!",
+            });
+          }
+        });
+      } else {
+        callback(500, {
+          error: "There was a problem in server side!",
+        });
+      }
+    });
+  } else {
+    callback(400, {
+      error: "You have a problem in your request",
+    });
+  }
 };
 
-handler._user.put = (requestProperties, callback) => {};
+handler._users.get = (requestProperties, callback) => {
+  callback(200);
+};
 
-handler._user.delete = (requestProperties, callback) => {};
+handler._users.put = (requestProperties, callback) => {};
+
+handler._users.delete = (requestProperties, callback) => {};
 
 module.exports = handler;
