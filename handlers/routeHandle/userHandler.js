1eb2629f981eb9c6aef3a8bec977a8a4b8bd3e33
al-jabir <aljabir098@gmail.com>
2022-11-22 22:21:49 +0600

    users.get

--- handlers/routeHandle/userHandler.js
+++ handlers/routeHandle/userHandler.js
@@ -89,7 +89,29 @@ handler._users.post = (requestProperties, callback) => {
 };
 
 handler._users.get = (requestProperties, callback) => {
-  callback(200);
+  const phone =
+    typeof requestProperties.queryStringObject.phone === "string" &&
+    requestProperties.queryStringObject.phone.trim().length === 11
+      ? requestProperties.queryStringObject.phone
+      : false;
+  if (phone) {
+    // lookup the user
+    data.read("users", phone, (err, u) => {
+      const user = { ...parseJSON(u) };
+      if (!err && user) {
+        delete user.password;
+        callback(200, user);
+      } else {
+        callback(404, {
+          error: "Requested user was not found!",
+        });
+      }
+    });
+  } else {
+    callback(404, {
+      error: "Requested user was not found!",
+    });
+  }
 };
 
 handler._users.put = (requestProperties, callback) => {};
