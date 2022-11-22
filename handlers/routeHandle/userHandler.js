07a79acc2a6fdbcaa22f3c23599984b0dae8853c
al-jabir <aljabir098@gmail.com>
2022-11-22 22:31:29 +0600

    users.delete

--- handlers/routeHandle/userHandler.js
+++ handlers/routeHandle/userHandler.js
@@ -88,105 +88,139 @@ handler._users.post = (requestProperties, callback) => {
   }
 };
 
-// handler._users.get = (requestProperties, callback) => {
-//   const phone =
-//     typeof requestProperties.queryStringObject.phone === "string" &&
-//     requestProperties.queryStringObject.phone.trim().length === 11
-//       ? requestProperties.queryStringObject.phone
-//       : false;
-//   if (phone) {
-//     // lookup the user
-//     data.read("users", phone, (err, u) => {
-//       const user = { ...parseJSON(u) };
-//       if (!err && user) {
-//         delete user.password;
-//         callback(200, user);
-//       } else {
-//         callback(404, {
-//           error: "Requested user was not found!",
-//         });
-//       }
-//     });
-//   } else {
-//     callback(404, {
-//       error: "Requested user was not found!",
-//     });
-//   }
-// };
+handler._users.get = (requestProperties, callback) => {
+  const phone =
+    typeof requestProperties.queryStringObject.phone === "string" &&
+    requestProperties.queryStringObject.phone.trim().length === 11
+      ? requestProperties.queryStringObject.phone
+      : false;
+  if (phone) {
+    // lookup the user
+    data.read("users", phone, (err, u) => {
+      const user = { ...parseJSON(u) };
+      if (!err && user) {
+        delete user.password;
+        callback(200, user);
+      } else {
+        callback(404, {
+          error: "Requested user was not found!",
+        });
+      }
+    });
+  } else {
+    callback(404, {
+      error: "Requested user was not found!",
+    });
+  }
+};
 
-// handler._users.put = (requestProperties, callback) => {
-//   // check the phone number if valid
-//   const phone =
-//     typeof requestProperties.body.phone === "string" &&
-//     requestProperties.body.phone.trim().length === 11
-//       ? requestProperties.body.phone
-//       : false;
+handler._users.put = (requestProperties, callback) => {
+  // check the phone number if valid
+  const phone =
+    typeof requestProperties.body.phone === "string" &&
+    requestProperties.body.phone.trim().length === 11
+      ? requestProperties.body.phone
+      : false;
 
-//   const firstName =
-//     typeof requestProperties.body.firstName === "string" &&
-//     requestProperties.body.firstName.trim().length > 0
-//       ? requestProperties.body.firstName
-//       : false;
+  const firstName =
+    typeof requestProperties.body.firstName === "string" &&
+    requestProperties.body.firstName.trim().length > 0
+      ? requestProperties.body.firstName
+      : false;
 
-//   const lastName =
-//     typeof requestProperties.body.lastName === "string" &&
-//     requestProperties.body.lastName.trim().length > 0
-//       ? requestProperties.body.lastName
-//       : false;
+  const lastName =
+    typeof requestProperties.body.lastName === "string" &&
+    requestProperties.body.lastName.trim().length > 0
+      ? requestProperties.body.lastName
+      : false;
 
-//   const password =
-//     typeof requestProperties.body.password === "string" &&
-//     requestProperties.body.password.trim().length > 0
-//       ? requestProperties.body.password
-//       : false;
+  const password =
+    typeof requestProperties.body.password === "string" &&
+    requestProperties.body.password.trim().length > 0
+      ? requestProperties.body.password
+      : false;
 
-//   if (phone) {
-//     if (firstName || lastName || password) {
-//       // loopkup the user
-//       data.read("users", phone, (err1, uData) => {
-//         const userData = { ...parseJSON(uData) };
+  if (phone) {
+    if (firstName || lastName || password) {
+      // loopkup the user
+      data.read("users", phone, (err1, uData) => {
+        const userData = { ...parseJSON(uData) };
 
-//         if (!err1 && userData) {
-//           if (firstName) {
-//             userData.firstName = firstName;
-//           }
-//           if (lastName) {
-//             userData.firstName = firstName;
-//           }
-//           if (password) {
-//             userData.password = hash(password);
-//           }
+        if (!err1 && userData) {
+          if (firstName) {
+            userData.firstName = firstName;
+          }
+          if (lastName) {
+            userData.firstName = firstName;
+          }
+          if (password) {
+            userData.password = hash(password);
+          }
 
-//           // store to database
-//           data.update("users", phone, userData, (err2) => {
-//             if (!err2) {
-//               callback(200, {
-//                 message: "User was updated successfully!",
-//               });
-//             } else {
-//               callback(500, {
-//                 error: "There was a problem in the server side!",
-//               });
-//             }
-//           });
-//         } else {
-//           callback(400, {
-//             error: "You have a problem in your request!",
-//           });
-//         }
-//       });
-//     } else {
-//       callback(400, {
-//         error: "You have a problem in your request!",
-//       });
-//     }
-//   } else {
-//     callback(400, {
-//       error: "Invalid phone number. Please try again!",
-//     });
-//   }
-// };
+          // store to database
+          data.update("users", phone, userData, (err2) => {
+            if (!err2) {
+              callback(200, {
+                message: "User was updated successfully!",
+              });
+            } else {
+              callback(500, {
+                error: "There was a problem in the server side!",
+              });
+            }
+          });
+        } else {
+          callback(400, {
+            error: "You have a problem in your request!",
+          });
+        }
+      });
+    } else {
+      callback(400, {
+        error: "You have a problem in your request!",
+      });
+    }
+  } else {
+    callback(400, {
+      error: "Invalid phone number. Please try again!",
+    });
+  }
+};
 
-handler._users.delete = (requestProperties, callback) => {};
+handler._users.delete = (requestProperties, callback) => {
+  // check the phone number if valid
+  const phone =
+    typeof requestProperties.queryStringObject.phone === "string" &&
+    requestProperties.queryStringObject.phone.trim().length === 11
+      ? requestProperties.queryStringObject.phone
+      : false;
+
+  if (phone) {
+    // lookup the user
+    data.read("users", phone, (err1, userData) => {
+      if (!err1 && userData) {
+        data.delete("users", phone, (err2) => {
+          if (!err2) {
+            callback(200, {
+              message: "User was successfully deleted!",
+            });
+          } else {
+            callback(500, {
+              error: "There was a server side error!",
+            });
+          }
+        });
+      } else {
+        callback(500, {
+          error: "There was a server side error!",
+        });
+      }
+    });
+  } else {
+    callback(400, {
+      error: "There was a problem in your request!",
+    });
+  }
+};
 
 module.exports = handler;
