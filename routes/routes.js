6295c34218c8575483f7a4f64392145b68cd28df
al-jabir <aljabir098@gmail.com>
2022-12-02 17:54:08 +0600

    tokenHandler

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,11 @@
 const { mafiaHandler } = require("../handlers/routeHandle/sampleHandler");
+const { tokenHandler } = require("../handlers/routeHandle/tokenHandler");
 const { userHandler } = require("../handlers/routeHandle/userHandler");
 const routes = {
   mafia: mafiaHandler,
   production: mafiaHandler,
   user: userHandler,
+  token: tokenHandler,
 };
 
 module.exports = routes;
